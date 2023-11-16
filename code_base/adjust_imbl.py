97dc2e16f2a47b6f10b87580b1d14c47beacff39
zxyao5148 <116566683+zxyao5148@users.noreply.github.com>
2023-11-16 16:54:50 +0800

    Update adjust_imbl.py
    enabled undersampling

--- code_base/adjust_imbl.py
+++ code_base/adjust_imbl.py
@@ -8,12 +8,17 @@
 # X_train_adj, y_train_adj = adjust_imbl(X_train, y_train)
 
 from collections import Counter
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 
-def adjust_imbl(X, y):
+def adjust_imbl(X, y, option='under'):
+    if option == 'over':
+        sampler = RandomOverSampler(random_state=42)
+    elif option == 'under':
+        sampler = RandomUnderSampler(random_state=42)
+    else:
+        raise ValueError("Invalid option parameter. \nPlease choose either 'over' or 'under'.")
     print('Original dataset shape %s' % Counter(y))
-    smo = SMOTE(random_state=42)
-    X_smo, y_smo = smo.fit_resample(X, y)
-    print('Resampled dataset shape %s' % Counter(y_smo))
-    return X_smo, y_smo
-
+    X_resampled, y_resampled = sampler.fit_resample(X, y)
+    print('Resampled dataset shape %s' % Counter(y_resampled))
+    return X_resampled, y_resampled
